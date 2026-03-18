4baab13a655e49c7183d23a4d3ce201b3632b437
sercinci <federico_ercole91@yahoo.it>
2026-03-18 18:11:24 +0100

    add 0cred verification id

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="0cred-verification" content="71308b8d-1604-4e7a-b3e8-fc49d5487664" />
+      </head>
       <body className={`${outfit.variable} antialiased bg-background text-foreground`}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
